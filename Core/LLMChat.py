4842f9edc5f9f1618eec9fd2e8012987f021fa16
Mohd Zaid <mdzaid0205@gmail.com>
2024-08-25 08:30:23 +0530

    ajfdsjgf

--- Core/LLMChat.py
+++ Core/LLMChat.py
@@ -1,12 +1,33 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama.llms import OllamaLLM
+from langchain_community.document_loaders import PyPDFLoader
 
 model = OllamaLLM(model="mistral")
 
+notesTemplate = """system:You are a notes generating program who generates notes based on context and available information to you
+tailored educational content for neuro-diverse students.
+provided below is pdf resource.
+resources: {resources}
+user:{question}
+"""
+
 systemTemplate = """system:You are a helpful assistant who provides tailored educational content for neuro-diverse students
-You provide concise statements in very short and precise answer.
+You provide concise statements in very short and precise answer based on context.
+context:{context}
 user:{question}"""
 
-def generate_response( task ):
-    response = model.invoke(systemTemplate.format(question=task))
+file_path = "Frontend\\uploads\\"
+
+def generate_response( task, context ):
+    print(systemTemplate.format(question=task, context=context))
+    response = model.invoke(systemTemplate.format(question=task, context=context))
     return response
+
+def generate_notes_from_pdf( name ):
+    loader = PyPDFLoader(file_path + "\\" + name)
+    docs = loader.load()
+    pdfContent = ""
+    for a, pages in enumerate(docs):
+        pdfContent = pdfContent + f'[Page {a}]' + pages.page_content;
+    note = model.invoke(notesTemplate.format(resources=pdfContent, question = "generate notes based on context, be precise and descriptive, use easy words"))
+    return note
--- Model/views.py
+++ Model/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from Core.LLMChat import generate_response
+from Core.LLMChat import generate_response, generate_notes_from_pdf
 from threading import Thread
 from dataclasses import dataclass
+from django.core.files.storage import FileSystemStorage
+
+Notes = "Unedited"
 
 # Create your views here.
 def homeView(request) -> HttpResponse:
     Question = None
     Answer = None
-    if request.method == 'POST':
+    if request.method == 'POST' or request.FILES.get('file'):
+        if request.FILES.get('file'):
+            file = request.FILES['file']
+            if file.content_type == 'application/pdf':
+                fs = FileSystemStorage(location='Frontend/uploads/')
+                filename = fs.save(file.name, file)
+                global Notes 
+                Notes = generate_notes_from_pdf(file.name)
+            else:
+                return HttpResponse("Only PDF files are allowed.")
         Question = request.POST.get('user_input')
-        Answer = generate_response(Question)
+        if Question not in ["None",None]:
+            Answer = generate_response(Question, Notes)
+        else:
+            Question = None
     return render(request,"index.html", {'question':Question,'answer':Answer})
